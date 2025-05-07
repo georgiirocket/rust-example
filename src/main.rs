f8fabda0700bfa136704183d00f7f27f044651e3
Rocket_GR <georgiiromanishko@gmail.com>
2025-05-07 09:47:22 +0200

    feat(project): var-const

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; //Not can change
     let mut x = 5; //Can change
     let m = 6; // Not can change
 
+    println!("Const {}", THREE_HOURS_IN_SECONDS);
     println!("This value is {}", x);
     println!("This m is {}", m);
     x = 6;
